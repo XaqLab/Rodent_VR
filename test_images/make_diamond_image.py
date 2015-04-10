767232dc598b7e1951a2a6252a8f46a15dcf6e5d
Jim Bridgewater <jwbwater@gmail.com>
2015-04-10 14:10:34 -0500

    Modified make_diamond_image.py.
    Changed it so that I can specify a list of pixels on which diamonds will be
    centered.  The resulting image is projected on the dome and a picture is taken.
    The picture of this image projected on the dome can be used to find the real
    forward mapping for these center pixels and then I can do a parameter search to
    find the parameter valuess that result in calculated directions for these
    pixels that match the real directions.

--- test_images/make_diamond_image.py
+++ test_images/make_diamond_image.py
@@ -2,92 +2,52 @@
 # written for python 2.7.8
 
 """
-Short script to create grid of diamonds image for dome projection calibration.
+Short script to create an image containing diamonds centered on specific pixels
+for dome projection calibration.
 """
 
 DEBUG = True
 
-from numpy import array, ones, zeros, dstack
+from numpy import array, ones, dstack
 from numpy import uint8, mod
 from PIL import Image
 
 
-def create_diamond_grid(diamond_spacing, diamond_size):
+def create_diamond_image(center_pixels, diamond_size):
     """
-    Make a checker board image with squares that have [rows, cols] of pixels
-    as specified by square_size and [rows, cols] of squares specified by 
-    board_size.
+    Make an image with diamonds centered on center_pixels.  The height and
+    width of the diamonds is specified by diamond_size.
     """
-    image_pixel_width = 1280
-    image_pixel_height = 720
 
-    #pixels = zeros([image_pixel_height, image_pixel_width, 3], dtype=uint8)
-    pixels = zeros([image_pixel_height, image_pixel_width], dtype=uint8)
+    IMAGE_PIXEL_WIDTH = 1280
+    IMAGE_PIXEL_HEIGHT = 720
+    DARK_PIXEL_VALUE = 0
+    LIGHT_PIXEL_VALUE = 192
 
-    def make_row_white(row):
-        """ Sub-function for making blank rows """
-        for col in range(image_pixel_width):
-            pixels[row][col] = 255
+    # make a dark background
+    pixels = ones([IMAGE_PIXEL_HEIGHT, IMAGE_PIXEL_WIDTH], dtype=uint8)
+    pixels = DARK_PIXEL_VALUE * pixels
 
-    row = 0
-    col = 0
+    half_size = (diamond_size - 1)/2
 
-    while row < image_pixel_height - diamond_spacing:
-        # white space rows
-        for i in range(row, row + diamond_spacing - diamond_size):
-            make_row_white(row)
-            row = row + 1
-    
-        # seed row
-        for col in range(image_pixel_width):
-            if (col + diamond_spacing/2.0) % diamond_spacing == 0:
-                pixels[row][col] = 0
-            else:
-                pixels[row][col] = 255
-        row = row + 1
-    
-        # grow from seed
-        for i in range((diamond_size + 1)/2 - 1):
-            for col in range(image_pixel_width):
-                if (pixels[row - 1][col] == 0):
-                    # pixel 1 row above is black
-                    pixels[row][col] = 0
-                elif (col > 0 and pixels[row - 1][col - 1] == 0):
-                    # pixel 1 row above and 1 row left is black
-                    pixels[row][col] = 0
-                elif (col < image_pixel_width - 1 and pixels[row - 1][col + 1] == 0):
-                    # pixel 1 row above and 1 row right is black
-                    pixels[row][col] = 0
-                else:
-                    pixels[row][col] = 255
-            row = row + 1
-    
-        # shrink from max width
-        for i in range((diamond_size + 1)/2 - 1):
-            for col in range(image_pixel_width):
-                if (col > 0 and col < image_pixel_width - 1 and
-                    pixels[row - 1][col - 1] == 0 and
-                    pixels[row - 1][col] == 0 and
-                    pixels[row - 1][col + 1] == 0):
-                    # 3 pixels above are black
-                    pixels[row][col] = 0
-                else:
-                    pixels[row][col] = 255
-            row = row + 1
-    
+    # add the diamonds
+    for center_pixel in center_pixels:
+        center_row = center_pixel[0]
+        center_col = center_pixel[1]
+        diamond_pixels = []
+        for row in range(center_row - half_size,
+                         center_row + half_size):
+            for col in range(center_col - half_size + abs(row - center_row),
+                             center_col + half_size - abs(row - center_row)):
+                pixels[row][col] = LIGHT_PIXEL_VALUE
 
-    while row < image_pixel_height:
-        # finish the image with white space rows
-        make_row_white(row)
-        row = row + 1
-
-
-    #return Image.fromarray(pixels, mode='RGB')
     return Image.fromarray(pixels, mode='L')
 
 
 if __name__ == "__main__":
-    checker_board_image = create_diamond_grid(60, 19)
+    diamond_size = 19
+    center_pixels = [[500, 540], [500, 740]]
+    checker_board_image = create_diamond_image(center_pixels, diamond_size)
     checker_board_image.save("diamonds.png")
 
 
