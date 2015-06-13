8ad9fc46d3bd077f37a339634b4adfe4b31c90df
Jim Bridgewater <jwbwater@gmail.com>
2015-06-13 17:13:52 -0400

    Added radial distortion parameters.

--- webcam.py
+++ webcam.py
@@ -11,3 +11,6 @@ theta = 0.5
 distance_to_screen = 3.0
 screen_width = 2 * distance_to_screen * tan(theta)
 screen_height = screen_width / (float(pixel_width) / float(pixel_height))
+
+# These were calculated by camera_calibration.py
+distortion_coefficients = [-1.17838063e-18, -9.44769420e-19, 2.13086476e-19]
