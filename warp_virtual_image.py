7fb7f561c20029b694f3571b81449a7e6df2cb21
Jim Bridgewater <jwbwater@gmail.com>
2017-01-06 14:16:19 -0600

    Changed warp_virtual_image.py to warp 3 images
    instead of 1.

--- warp_virtual_image.py
+++ warp_virtual_image.py
@@ -6,6 +6,21 @@ from dome_projection import DomeProjection
 import sys
 import re
 
+"""
+Parameters from the rat VR system
+
+Animal_position: 0.0 -0.139974099238 0.546942519452
+Dome_center: 0.0 0.0148932688144 0.46755637477
+Dome_radius: 0.541409227296
+Mirror_radius: 0.222935706293
+Projector_focal_point: 0.0 0.84950020408 -0.00296533955158
+Projector_roll: -0.0209584381456
+Projector_theta: 0.269020062357
+Projector_vertical_offset: 0.183968098375
+
+Width:Height:Viewing Distance=1.4:1:0.5
+"""
+
 DEBUG = False
 
 dome_image_size = (0, 0)
@@ -25,22 +40,27 @@ for filepath in sys.argv[1:]:
         dome_image_size = input_image.size
         # Initialize the dome display
         print "Creating instance of DomeProjection Class"
-                              #screen_height = [2.375],
-                              #screen_width = [4.0625],
-                              #distance_to_screen = [3.0],
         dome = DomeProjection(
-                              screen_height = [dome_image_size[1]],
-                              screen_width = [dome_image_size[0]],
-                              distance_to_screen = [2.5*dome_image_size[1]],
-                              pitch = [10],
-                              yaw = [0],
-                              image_pixel_width=[dome_image_size[0]],
-                              image_pixel_height=[dome_image_size[1]],
-                              projector_pixel_width=1280,
-                              projector_pixel_height=720)
+            animal_position = [0.0, -0.139974099238, 0.546942519452],
+            dome_center = [0.0, 0.0148932688144, 0.46755637477],
+            dome_radius = 0.541409227296,
+            mirror_radius = 0.222935706293,
+            projector_focal_point = [0.0, 0.84950020408, -0.00296533955158],
+            projector_roll = -0.0209584381456,
+            projector_theta = 0.269020062357,
+            projector_vertical_offset = 0.183968098375,
+            screen_height = [dome_image_size[1]]*3,
+            screen_width = [dome_image_size[0]]*3,
+            distance_to_screen = [0.3*dome_image_size[1]]*3,
+            pitch = [10]*3,
+            yaw = [-90, 0, 90],
+            image_pixel_width=[dome_image_size[0]]*3,
+            image_pixel_height=[dome_image_size[1]]*3,
+            projector_pixel_width=1920,
+            projector_pixel_height=1080)
         print "Done initializing dome"
     
-    output_image = dome.warp_image_for_dome([input_image])
+    output_image = dome.warp_image_for_dome([input_image]*3)
     
     # save warped image
     output_image.save(directory + "warped_" + filename, filetype)
