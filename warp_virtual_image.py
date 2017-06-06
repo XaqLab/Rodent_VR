32f10acd7cba8c8eaa1f1775148d0437c6e0f376
Jim Bridgewater <jwbwater@gmail.com>
2017-06-06 15:51:09 -0500

    Added choice between rat and mouse VR systems.

--- warp_virtual_image.py
+++ warp_virtual_image.py
@@ -6,28 +6,64 @@ from dome_projection import DomeProjection
 import sys
 import re
 
-"""
-Parameters from the rat VR system
-
-Animal_position: 0.0 -0.139974099238 0.546942519452
-Dome_center: 0.0 0.0148932688144 0.46755637477
-Dome_radius: 0.541409227296
-Mirror_radius: 0.222935706293
-Projector_focal_point: 0.0 0.84950020408 -0.00296533955158
-Projector_roll: -0.0209584381456
-Projector_theta: 0.269020062357
-Projector_vertical_offset: 0.183968098375
-
-Width:Height:Viewing Distance=1.4:1:0.5
-"""
-
 DEBUG = False
+
+# Choose a VR system, rat or mouse.
+#system = "rat"
+system = "mouse"
+
+# Choose the number of images and corresponding yaw angles for each.
+num_images = 1; yaw = [0]             # one image in front
+#num_images = 3; yaw = [-90, 0, 90]   # three images left, center, right
+
+if system == "rat":
+    # Use parameters for the rat VR system.
+    parameters = lambda image_size, num_images, yaw:{
+        "animal_position": [0.0, -0.139974099238, 0.546942519452],
+        "dome_center": [0.0, 0.0148932688144, 0.46755637477],
+        "dome_radius": 0.541409227296,
+        "mirror_radius": 0.222935706293,
+        "projector_focal_point": [0.0, 0.84950020408, -0.00296533955158],
+        "projector_roll": -0.0209584381456,
+        "projector_theta": 0.269020062357,
+        "projector_vertical_offset": 0.183968098375,
+        "screen_height": [image_size[1]]*num_images,
+        "screen_width": [image_size[0]]*num_images,
+        "distance_to_screen": [0.3*image_size[1]]*num_images,
+        "pitch": [10]*num_images,
+        "yaw": yaw,
+        "image_pixel_width": [image_size[0]]*3,
+        "image_pixel_height": [image_size[1]]*3,
+        "projector_pixel_width": 1920,
+        "projector_pixel_height": 1080
+    }
+elif system == "mouse":
+    # Use parameters for the mouse VR system.
+    parameters = lambda image_size, num_images, yaw:{
+        "animal_position": [0, 0.056, 0.575],
+        "dome_center": [0, 0.110, 0.348],
+        "dome_radius": 0.601,
+        "mirror_radius": 0.162,
+        "projector_focal_point": [0, 0.858, -0.052],
+        "projector_roll": 0.01,
+        "projector_theta": 0.166,
+        "projector_vertical_offset": 0.211,
+        "screen_height": [image_size[1]]*num_images,
+        "screen_width": [image_size[0]]*num_images,
+        "distance_to_screen": [2.5*image_size[1]]*num_images,
+        "pitch": [10]*num_images,
+        "yaw": yaw,
+        "image_pixel_width": [image_size[0]]*3,
+        "image_pixel_height": [image_size[1]]*3,
+        "projector_pixel_width": 1280,
+        "projector_pixel_height": 720
+    }
 
 dome_image_size = (0, 0)
 # Warp each file passed as an argument for projection on the dome
 for filepath in sys.argv[1:]:
     filename = re.split(r"/",filepath)[-1]
-    if "jpg" in filename.lower():
+    if "jpg" in filename.lower() or "jpeg" in filename.lower():
         filetype = "jpeg"
     elif "png" in filename.lower():
         filetype = "png"
@@ -40,24 +76,7 @@ for filepath in sys.argv[1:]:
         dome_image_size = input_image.size
         # Initialize the dome display
         print "Creating instance of DomeProjection Class"
-        dome = DomeProjection(
-            animal_position = [0.0, -0.139974099238, 0.546942519452],
-            dome_center = [0.0, 0.0148932688144, 0.46755637477],
-            dome_radius = 0.541409227296,
-            mirror_radius = 0.222935706293,
-            projector_focal_point = [0.0, 0.84950020408, -0.00296533955158],
-            projector_roll = -0.0209584381456,
-            projector_theta = 0.269020062357,
-            projector_vertical_offset = 0.183968098375,
-            screen_height = [dome_image_size[1]]*3,
-            screen_width = [dome_image_size[0]]*3,
-            distance_to_screen = [0.3*dome_image_size[1]]*3,
-            pitch = [10]*3,
-            yaw = [-90, 0, 90],
-            image_pixel_width=[dome_image_size[0]]*3,
-            image_pixel_height=[dome_image_size[1]]*3,
-            projector_pixel_width=1920,
-            projector_pixel_height=1080)
+        dome = DomeProjection(**parameters(dome_image_size, num_images, yaw))
         print "Done initializing dome"
     
     output_image = dome.warp_image_for_dome([input_image]*3)
