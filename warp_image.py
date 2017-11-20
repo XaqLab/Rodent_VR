cfe5a44c3d2a64c0c4580bde90c7328f347757d5
Jim Bridgewater <jwbwater@gmail.com>
2017-11-20 13:12:00 -0600

    Simplified warp_image.py. Removed some options
    that were specific to Dora's lab.

--- warp_image.py
+++ warp_image.py
@@ -6,58 +6,34 @@ from dome_projection import DomeProjection
 import sys
 import re
 
+# if DEBUG is True then unwarp the image to show what it should look like
+# inside the VR system.
 DEBUG = True
-
-# Choose a VR system, rat or mouse.
-#system = "rat"
-system = "mouse"
 
 # Choose the number of images and corresponding yaw angles for each.
 num_images = 1; yaw = [0]             # one image in front
 #num_images = 3; yaw = [-90, 0, 90]   # three images left, center, right
 
-if system == "rat":
-    # Use parameters for the rat VR system.
-    parameters = lambda image_size, num_images, yaw:{
-        "animal_position": [0.0, -0.139974099238, 0.546942519452],
-        "dome_center": [0.0, 0.0148932688144, 0.46755637477],
-        "dome_radius": 0.541409227296,
-        "mirror_radius": 0.222935706293,
-        "projector_focal_point": [0.0, 0.84950020408, -0.00296533955158],
-        "projector_roll": -0.0209584381456,
-        "projector_theta": 0.269020062357,
-        "projector_vertical_offset": 0.183968098375,
-        "screen_height": [image_size[1]]*num_images,
-        "screen_width": [image_size[0]]*num_images,
-        "distance_to_screen": [0.3*image_size[1]]*num_images,
-        "pitch": [10]*num_images,
-        "yaw": yaw,
-        "image_pixel_width": [image_size[0]]*num_images,
-        "image_pixel_height": [image_size[1]]*num_images,
-        "projector_pixel_width": 1920,
-        "projector_pixel_height": 1080
-    }
-elif system == "mouse":
-    # Use parameters for the mouse VR system.
-    parameters = lambda image_size, num_images, yaw:{
-        "animal_position": [0, 0.056, 0.575],
-        "dome_center": [0, 0.110, 0.348],
-        "dome_radius": 0.601,
-        "mirror_radius": 0.162,
-        "projector_focal_point": [0, 0.858, -0.052],
-        "projector_roll": 0.01,
-        "projector_theta": 0.166,
-        "projector_vertical_offset": 0.211,
-        "screen_height": [image_size[1]]*num_images,
-        "screen_width": [image_size[0]]*num_images,
-        "distance_to_screen": [1.0*image_size[1]]*num_images,
-        "pitch": [10]*num_images,
-        "yaw": yaw,
-        "image_pixel_width": [image_size[0]]*num_images,
-        "image_pixel_height": [image_size[1]]*num_images,
-        "projector_pixel_width": 1280,
-        "projector_pixel_height": 720
-    }
+# Adjust these parameters for your VR system.
+parameters = lambda image_size, num_images, yaw:{
+    "animal_position": [0, 0.056, 0.575],
+    "dome_center": [0, 0.110, 0.348],
+    "dome_radius": 0.601,
+    "mirror_radius": 0.162,
+    "projector_focal_point": [0, 0.858, -0.052],
+    "projector_roll": 0.01,
+    "projector_theta": 0.166,
+    "projector_vertical_offset": 0.211,
+    "screen_height": [image_size[1]]*num_images,
+    "screen_width": [image_size[0]]*num_images,
+    "distance_to_screen": [1.0*image_size[1]]*num_images,
+    "pitch": [10]*num_images,
+    "yaw": yaw,
+    "image_pixel_width": [image_size[0]]*num_images,
+    "image_pixel_height": [image_size[1]]*num_images,
+    "projector_pixel_width": 1280,
+    "projector_pixel_height": 720
+}
 
 dome_image_size = (0, 0)
 # Warp each file passed as an argument for projection on the dome
@@ -75,10 +51,10 @@ for filepath in sys.argv[1:]:
     if dome_image_size != input_image.size:
         dome_image_size = input_image.size
         # Initialize the dome display
-        print "Creating instance of DomeProjection Class"
         dome = DomeProjection(**parameters(dome_image_size, num_images, yaw))
-        print "Done initializing dome"
     
+    print "Building the lookup table and they modifying the image."
+    print "Building the lookup table will take a few minutes."
     output_image = dome.warp_image_for_dome([input_image]*num_images)
     
     # save warped image
